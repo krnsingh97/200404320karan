89abfbaef0d0ace178c01913bc5344c29edcfe67
singhatwalkaran@outlook.com <singhatwalkaran@outlook.com>
2019-05-29 19:47:35 -0400

    oyehoja

---
+++ 200404320karan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200404320karan.Startup))]
+namespace _200404320karan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
