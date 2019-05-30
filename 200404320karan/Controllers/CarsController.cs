424679c10c3737e78c40ed15171c07ca35016789
singhatwalkaran@outlook.com <singhatwalkaran@outlook.com>
2019-05-29 21:53:06 -0400

    all

--- 200404320karan/Controllers/CarsController.cs
+++ 200404320karan/Controllers/CarsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _200404320karan.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,12 @@ namespace _200404320karan.Controllers
     public class CarsController : Controller
     {
         // GET: Cars
-        public ActionResult Index()
+        public ActionResult mycar()
         {
+            Gaddi alex = new Gaddi();
+            alex.FirstName = "alws";
+            alex.LastName = " daawd";
+       
             return View();
         }
     }
---
+++ 200404320karan/Models/Gaddi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200404320karan.Models
+{
+    public class Gaddi
+    {
+        public int studentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
